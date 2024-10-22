4401c36485b4666622aa400b27ef1b313d1c7842
Joe Aguado <aguado.joe@gmail.com>
2024-10-22 16:12:45 -0500

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+import biob from "@/public/biob.png"
+import nts from "@/public/nts.png"
+import sweat from "@/public/sweat.png"
+
+export const links = [
+    {
+        name: "Home",
+        hash: "#home",
+    },
+    {
+        name: "About",
+        hash: "#about"
+    },
+    {
+        name: "Case Studies",
+        hash: "#studies"
+    },
+    {
+        name: "Projects",
+        hash: "#projects"
+    },
+    {
+        name: "Contact",
+        hash: "#projects"
+    }
+]
